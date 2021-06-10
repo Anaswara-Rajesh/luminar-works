9c7d3e39d8dca6c6327356bf6d4e3ce68fdb8717
Anaswara <anaswararajesh45@gmail.com>
2021-06-10 19:53:10 +0530

    adding calculator program

---
+++ javascript_pgms/calculator.js
@@ -0,0 +1,8 @@
+var num1=10;
+var num2=5;
+console.log(`Number1= ${num1}  Number2= ${num2}`)
+console.log(`addition= ${num1+num2}`);
+console.log(`substraction= ${num1-num2}`);
+console.log(`multiplication= ${num1*num2}`);
+console.log(`division= ${num1/num2}`);
+console.log(`modulus= ${num1%num2}`);
